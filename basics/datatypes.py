07f54f05fc8fcf6906d3d052efca3bc49ac7601b
Adam Scanga <sandmanscanga@gmail.com>
2021-02-19 18:51:38 -0600

    Updated the datatypes.py file

--- basics/datatypes.py
+++ basics/datatypes.py
@@ -10,21 +10,21 @@ string = "This is a string"  # TYPE: str
 integer = 1024               # TYPE: int
 decimal = 3.14               # TYPE: float
 
-# print(type(string))
-# print(type(integer))
-# print(type(decimal))
-# # <class 'str'>
-# # <class 'int'>
-# # <class 'float'>
+print(type(string))
+print(type(integer))
+print(type(decimal))
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
 
-# print(type(str(123.426537)))
-# print(type(int(3.14)))
-# print(type(float(3)))
-# print(type(int("132312")))
-# print(type(float("132312.32576")))
-# # <class 'str'>
-# # <class 'int'>
-# # <class 'float'>
-# # <class 'int'>
-# # <class 'float'>
+print(type(str(123.426537)))
+print(type(int(3.14)))
+print(type(float(3)))
+print(type(int("132312")))
+print(type(float("132312.32576")))
+# <class 'str'>
+# <class 'int'>
+# <class 'float'>
+# <class 'int'>
+# <class 'float'>
 
